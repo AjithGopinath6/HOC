f191d3852b9e182b129511690a9b06ad1ecfee9c
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-10-02 13:40:12 +0530

    get values from  input data by the user typed. here we use the same method in both form. to over come this we implement HOC

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import { Button } from 'react-bootstrap'
 
+
 function ContactForm() {
+    const[formData, setFormData] = useState({
+        name: '',
+        contact: '',
+        address: ''
+    })
+
+    const handleChange = (e)=>{
+        const {name, value} = e.target;
+        setFormData((prevData)=>
+            ({...prevData, [name]: value}
+        ))
+    }
+
+    const handleSubmit =(e)=>{
+        e,preventDefault();
+        console.log('formData:', formData);
+    }
+
   return (
     <Container>
         <div className='service'>
             
-            <form action="" className='form'>
+            <form action="" className='form' onClick={handleSubmit}>
             <label for="name">Customer Name :</label>
-                <input type="text" />
+                <input onChange={handleChange} type="text" name='name' />
                 <label for="contact">Contact Number :</label>
-                <input type="tel" id="contact" name="contact" pattern="[0-9]{10}" required/>
-                <label>Address :</label> <textarea id="address" name="address" rows="2" cols="40" ></textarea>
+                <input onChange={handleChange} type="tel" id="contact" name="contact" pattern="[0-9]{10}" required/>
+                <label>Address :</label> <textarea onChange={handleChange} id="address" name="address" rows="2" cols="40" ></textarea>
                 <Button className='btn' type='submit'>Submit</Button>
             </form>
             
--- src/Components/ServiceForm.jsx
+++ src/Components/ServiceForm.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
 
 function ServiceForm() {
+    const[formData,setFormData]=useState({
+        name:'',
+        empid:'',
+        message:''
+    });
+
+// function to handle changes.
+    const handleChange=(e)=>{
+        const{name, value} = e.target;
+        setFormData((prevData)=>({
+            ...prevData, [name]: value,
+        }));
+    }
+
+    // function to handle from submission.
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log('formData:',formData);
+    }
+
   return (
     <Container>
         <div className='service'>
             
-            <form action="" className='form'>
+            <form action="" className='form' onSubmit={handleSubmit}>
             <label for="name">Service Emp Name:</label>
-                <input type="text" />
+                <input onChange={handleChange} name='name' type="text" />
                 <label>Emp Id :</label> 
-                <input type="tel" id="contact" name="contact" pattern="[0-9]{5}" required/>
-                <label>Feedback :</label> <textarea id="message" name="message" rows="2" cols="40" placeholder="Enter your Feedback here..."></textarea>
+                <input onChange={handleChange} type="tel" id="empid" name="empid" pattern="[0-9]{3}" required/>
+                <label>Feedback :</label>
+                 <textarea onChange={handleChange} id="message" name="message" rows="2" cols="40" placeholder="Enter your Feedback here..."></textarea>
                 <Button className='btn' type='submit'>Submit</Button>
             </form>
             
