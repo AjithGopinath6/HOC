6eb241e3664bfac8f9b0ddced4b5278ba9ff96a7
Ajith Gopinath <ajithgopinath639@gmail.com>
2024-10-02 13:58:17 +0530

    we just created an hoc. and add the additional functionalities for the component

---
+++ src/Components/WithTrack.jsx
@@ -0,0 +1,33 @@
+// higher order component(HOC)
+//  it is a pattern used to reuse component logic. An HOC is a function that takes a component as an argument and returns a new component with additional props or functionality
+
+import React from 'react'
+    // here we take a component as an argument we can give any name to it. indefault we give "WrappedComponent"
+function WithTrack(WrappedComponent) {
+    // in an HOC there is 2 return statements.
+  return ()=>{
+    // here we return the additional functionalities that the original component wants.we give it in the first return.
+    const[formData, setFormData] = useState({
+        name: '',
+        contact: '',
+        address: ''
+    })
+
+    const handleChange = (e)=>{
+        const {name, value} = e.target;
+        setFormData((prevData)=>
+            ({...prevData, [name]: value}
+        ))
+    }
+
+    const handleSubmit =(e)=>{
+        e,preventDefault();
+        console.log('formData:', formData);
+    }
+    // here we return the component
+    return 
+        <WrappedComponent/>
+  }
+}
+
+export default WithTrack
